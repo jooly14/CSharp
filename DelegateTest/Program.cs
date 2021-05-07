8a475cb0456a338c5256bc05260263370da25f04
jooly14 <69379312+jooly14@users.noreply.github.com>
2021-05-07 17:15:15 +0900

    Add files via upload

---
+++ DelegateTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DelegateTest
+{
+    delegate void Del();
+    class Program
+    {
+        public static void Print()
+        {
+            Console.WriteLine("대리자를 통한 메서드 호출");
+        }   
+
+        public static void Print2()
+        {
+            Console.WriteLine("하나 더 참조");
+        }
+        public static void Main()
+        {
+            Del myDel = new Del(Print);
+            myDel += Print2;
+            myDel();
+        }
+    }
+}
