8221d4850e090e830c0fb7e561733202a554a908
jooly14 <69379312+jooly14@users.noreply.github.com>
2021-05-07 17:10:44 +0900

    Add files via upload

---
+++ BinaryWriterPrj/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace BinaryWriterPrj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (BinaryWriter bw = new BinaryWriter(new FileStream("test.dat", FileMode.Create)))
+            {
+                bw.Write(12);
+                bw.Write(3.14f);
+                bw.Write("Hello World!!");
+            }
+        }
+    }
+}
---
+++ ConsoleClassPrj/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleClassPrj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(100, 40);
+            Random ran = new Random();
+            ConsoleColor[] colors = {ConsoleColor.Blue, ConsoleColor.Red, ConsoleColor.Cyan, ConsoleColor.Yellow, ConsoleColor.Green, ConsoleColor.Magenta, ConsoleColor.Gray };
+            
+            while (true)
+            {
+                Console.Clear();
+                for(int i = 0; i < 30; i++)
+                {
+                    Console.ForegroundColor = colors[ran.Next(7)];
+                    Console.SetCursorPosition(ran.Next(100), ran.Next(40));
+                    Console.Write("Hello World");
+                    
+                }
+                Thread.Sleep(300);
+            }
+        }
+    }
+}
