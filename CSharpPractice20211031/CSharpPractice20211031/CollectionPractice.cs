4d185b575843da2609fe1a308602e586f794a88e
jooly14 <69379312+jooly14@users.noreply.github.com>
2021-10-31 20:38:58 +0900

    20211031 CSharp 컬렉션 LINQ

---
+++ CSharpPractice20211031/CSharpPractice20211031/CollectionPractice.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpPractice20211031
+{
+    class CollectionPractice
+    {
+        static void Main(string[] args)
+        {
+            //Main1();
+            //Main2();
+            //Main3();
+            Main4();
+        }
+
+        //LIst Class
+        static void Main1()
+        {
+            List<String> colors = new List<string>();
+            colors.Add("Red");
+            colors.Add("Green");
+            colors.Add("Blue");
+            for (int i = 0; i < colors.Count; i++)
+            {
+                Console.WriteLine(colors[i]);
+            }
+        }
+        //Enumerable Class
+        static void Main2()
+        {
+            var numbers = Enumerable.Range(1,5);
+            foreach (var item in numbers)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine();
+        }
+
+        //Dictionary Class
+        static void Main3()
+        {
+            var data = new Dictionary<string, string>();
+            data.Add("Eng","Hello");
+            data.Add("Kor", "안녕");
+            foreach (var item in data)
+            {
+                Console.WriteLine($"Key : {item.Key} / Value : {item.Value}");
+            }
+        }
+
+        // Nullable Type
+        static void Main4()
+        {
+            int? nullableInt = null;
+            int intValue = nullableInt ?? default; // ?? null값을 가지고 있는 경우 오른쪽 값을 반환
+            Console.WriteLine($"{intValue}");
+
+            var retval = nullableInt?.CompareTo(1) ?? -1; // ? 해당 변수의 값이 null인 경우 오른쪽 연산을 실행하지 않고
+                                                          // null 값을 반환한다. 이 경우 ?? 연산자를 사용하여 반환값이
+                                                          // null인 경우 정해진 값을 반환하도록 할 수 있다.
+                                                          // 그런 점에서 ?연산자와 ??연산자를 함께 사용하면 좋다.
+            Console.WriteLine(retval);
+        }
+    }
+}
---
+++ CSharpPractice20211031/LinqPractice/LinqPractice.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqPractice
+{
+    class LinqPractice
+    {
+        static void Main(string[] args)
+        {
+            //Main1();
+            Main2();
+        }
+        //List Array Class 활용
+        static void Main1()
+        {
+            List<int> list1 = new List<int>();
+            list1.Add(1);
+            list1.Add(2);
+            list1.Add(3);
+            list1.Add(4);
+            int[] arr1 = list1.ToArray();
+            Array.Reverse(arr1);
+            list1 = new List<int>(arr1);
+            foreach (var item in list1)
+            {
+                Console.WriteLine(item.ToString());
+
+            }
+        }
+        static void Main2()
+        {
+            List<int> list1 = new List<int>(new int[]{ 1, 2, 3 });
+            int sum = list1.Sum();
+            Console.WriteLine($"sum : {sum}");
+            int count = list1.Count();
+            Console.WriteLine($"count : {count}");
+            double avg = list1.Average();
+            Console.WriteLine($"average : {avg}");
+            int max = list1.Max();
+            Console.WriteLine($"max : {max}");
+            int min = list1.Min();
+            Console.WriteLine($"min : {min}");
+            IEnumerable<int> ie = list1.Where(num => num > 2);
+            foreach (var item in ie)
+            {
+                Console.Write(item+"\t");
+            }
+            Console.WriteLine();
+            list1 = ie.ToList<int>();
+            Console.WriteLine(list1.Count);
+
+            IEnumerable<int> ie2 = Enumerable.Range(1,10);
+            int sum2 = ie2.Where(x => x % 2 == 0).Sum();
+            Console.WriteLine($"sum2 : {sum2}");
+            int sum3 = ie2.Sum(x => x % 2 == 0? x : 0);
+            Console.WriteLine($"sum3 : {sum3}");
+
+            bool bln1 = ie2.Any(x => x == 2);
+            Console.WriteLine(bln1? $"2 is exist": $"2 is not exist");
+        }
+    }
+}
